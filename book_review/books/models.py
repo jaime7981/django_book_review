aa57eb9a71522a9f03c61b371c8cb19bc0f0a593
jaime7981 <jmfernandez1@miaundes.cl>
2023-08-03 17:11:45 -0400

    models

--- book_review/books/models.py
+++ book_review/books/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    summary = models.TextField()
+    publish_date = models.DateField()
+
+    author = models.ForeignKey(
+        'Author', 
+        on_delete=models.CASCADE
+    )
+    
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    description = models.TextField()
+    
+    country = models.ForeignKey(
+        'Country', 
+        on_delete=models.SET_NULL
+    )
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+class Review(models.Model):
+    text = models.TextField()
+    rating = models.IntegerField()
+    upvotes = models.IntegerField()
+    date = models.DateField()
+
+    book = models.ForeignKey(
+        'Book', 
+        on_delete=models.CASCADE
+    )
+
+class Sales(models.Model):
+    date = models.DateField()
+    amount = models.IntegerField()
+
+    book = models.ForeignKey(
+        'Book',
+        on_delete=models.CASCADE
+    )
