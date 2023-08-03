37c6ae1a1ac7d6c060e8e515828497b4543e8e1e
jaime7981 <jmfernandez1@miaundes.cl>
2023-08-03 18:24:32 -0400

    seed data with books reviews and sales

--- book_review/seed_data.py
+++ book_review/seed_data.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import django
 import random
@@ -14,6 +15,7 @@ from books.models import Book, Author, Country, Review, Sales
 
 #Set Faker
 fake = Faker()
+
 
 def delete_data():
     for country in Country.objects.all():
@@ -33,13 +35,14 @@ def delete_data():
     
     print('data deleted')
 
+
 def create_countries():
     for country_number in range(21):
         country, created = Country.objects.get_or_create(
             name=fake.country()
         )
-        #new_country.save()
     print('countries created')
+
 
 def create_authors():
     countries = Country.objects.all()
@@ -51,12 +54,50 @@ def create_authors():
             country=random.choice(countries)
         )
         author.save()
+        create_books_by_authors(author)
+
+
+def create_books_by_authors(author):
+    for book_number in range(10):
+        book = Book.objects.create(
+            name=fake.sentence(),
+            summary=fake.text(),
+            publish_date=fake.date(),
+            author=author
+        )
+        book.save()
+        create_reviews_by_books(book)
+        create_sales_by_books(book)
+
+
+def create_reviews_by_books(book):
+    for review_number in range(random.randint(4, 10)):
+        review = Review.objects.create(
+            text=fake.text(),
+            rating=random.randint(1, 5),
+            upvotes=random.randint(1, 100),
+            date=fake.date(),
+            book=book
+        )
+        review.save()
+
+
+def create_sales_by_books(book):
+    for sales_year in range(2015, 2023):
+        sales = Sales.objects.create(
+            date=datetime(sales_year, random.randint(1,12), random.randint(1,28)),
+            amount=random.randint(100, 1000),
+            book=book
+        )
+        sales.save()
+    
 
 def seed_data():
     print("Seeding data...")
     delete_data()
     create_countries()
     create_authors()
+
 
 if __name__ == '__main__':
     seed_data()
