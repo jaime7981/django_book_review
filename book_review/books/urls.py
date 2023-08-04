3284fd80c4805407addee68646636e839aed64e1
jaime7981 <jmfernandez1@miaundes.cl>
2023-08-04 12:11:00 -0400

    first crud aproach for authors

--- book_review/books/urls.py
+++ book_review/books/urls.py
@@ -5,4 +5,6 @@ from . import views as books
 urlpatterns = [
     path('', RedirectView.as_view(url='/books/main/')),
     path('main/', books.main, name = 'main'),
+    path('author/', books.author, name = 'authors'),
+    path('author/<int:author_id>/', books.author, name = 'author'),
 ]
--- book_review/books/views.py
+++ book_review/books/views.py
@@ -5,3 +5,11 @@ from .models import Book, Author, Country, Review, Sales
 def main(request):
     authors = Author.objects.all()
     return render(request, 'main.html', context={'authors': authors})
+
+def author(request, author_id=None):
+    if author_id == None:
+        authors = Author.objects.all()
+        return render(request, 'authors.html', context={'authors': authors})
+    else:
+        author = Author.objects.get(id=author_id)
+        return render(request, 'author.html', context={'author': author})
