f479f4c5f816c9c29de87b09e3fd48a60ebbf1a1
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-08-05 21:16:40 -0400

    add authors info view

--- book_review/core/views.py
+++ book_review/core/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from django.db.models import Count, Avg, Sum
+from core.models import Author, Book
 
 
 # Create your views here.
 def main(request):
     return render(request, 'main.html')
+
+
+def authorsInfo(request):
+    # A table that shows authors, number of published books, average score
+    # and total sales. This table should have a sort and filter for each
+    # column.
+
+    authors = Author.objects.annotate(
+        books_count=Count('book'),
+        avg_score=Avg('book__review__rating'),
+        total_sales=Sum('book__sales__amount')
+    )
+
+    return render(request, 'authors_info.html', {'authors_info': authors})
